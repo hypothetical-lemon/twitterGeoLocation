ef361882895d1173704a55441631124a98dfef25
Heather Lemon <mcmillenh90@gmail.com>
2018-06-30 17:20:20 -0600

    add config

--- twitterConfig.py
+++ twitterConfig.py
@@ -3,11 +3,10 @@
 class TwitterConfig():
 
     def __init__(self):
-        # Read in from file?  or take in additional params for constructions
-        self.access_key = "572118432-qBMhyhE48biyzrRHvYn8kjt6LvTKTvFt00hTnAHx"
-        self.access_secret = "5JlEPxwTYunT0wF4aoGLJE40j8v67Gwx6BixjRt8FGQO1"
-        self.consumer_key='NcWE9P5UP9UlhVJibPh3cVKxh'
-        self.consumer_secret='xdSYoNZ2n9iaXXwiag5OFzQS74cTCYPUKwaulqENjVvs6BE5ec'
+         self.access_key = "Access Key"
+        self.access_secret = "Access Secret"
+        self.consumer_key='Consumer Key'
+        self.consumer_secret='Consumer Secret'
         
 
     def verifyTwitter(self): 
