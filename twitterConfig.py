f384d236e823cadb56076d775d36de4284892576
Heather Lemon <mcmillenh90@gmail.com>
2018-07-01 14:35:35 -0600

    config.json setup

--- twitterConfig.py
+++ twitterConfig.py
@@ -1,13 +1,24 @@
 #!/usr.bin.python
+import json
+
 
 class TwitterConfig():
-    # write keys
-    def __init__(self):
-         self.access_key = "Access Key"
-        self.access_secret = "Access Secret"
-        self.consumer_key='Consumer Key'
-        self.consumer_secret='Consumer Secret'
-        
 
-    def verifyTwitter(self): 
-        print(self.twitter.VerifyCredentials())
+    def __init__(self):
+        # Read in from file?  or take in additional params for constructions
+        try:
+            with open('config.json', 'r') as json_data_file:
+                config = json.load(json_data_file)
+
+                self.access_key = config['twitter_key']['access_key']
+                self.access_secret = config['twitter_key']['access_secret']
+                self.consumer_key = config['twitter_key']['consumer_key']
+                self.consumer_secret = config['twitter_key']['consumer_secret']
+        finally:
+            json_data_file.close() 
+        
+def main():
+    twitterConfig = TwitterConfig()
+
+if __name__ == "__main__":
+    main()
